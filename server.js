7b0be6ad11f693c1031df55bb9b5ca3adbdb1905
katorsyforstudy <katorsyforstudy@gmail.com>
2026-05-12 14:59:02 +0300

    поправки

--- server.js
+++ server.js
@@ -70,6 +70,8 @@ app.get("/bookings", (req, res) => {
 // 🏠 Сервируем статику
 app.use(express.static(path.join(__dirname, ".")));
 
-app.listen(3000, () => {
-  console.log("🚀 Server started: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server started: http://localhost:${PORT}`);
 });
